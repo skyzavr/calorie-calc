965a024c751dff7b3e5b2c3a602e84dbf5684ed5
nino <dankozero111@gmail.com>
2024-10-28 22:58:20 +0300

    [feat]: add main layout for app router

---
+++ src/app/layout/MainLayout.tsx
@@ -0,0 +1,10 @@
+import { Header } from '@widgets/header';
+import { Layout } from '@shared/ui/Layout/Layout';
+
+export const MainLayout = () => {
+  return (
+    <Layout>
+      <Header />
+    </Layout>
+  );
+};
