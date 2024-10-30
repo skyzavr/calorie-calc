79208922e69369f6c9968071cef351c52d983b77
nino <dankozero111@gmail.com>
2024-10-30 14:20:41 +0300

    [feat]: add toggle button group component

---
+++ src/entities/toggleButtonGroup/index.ts
@@ -0,0 +1,1 @@
+export { ToggleButtonGroupComponent as ToggleGroup } from './ui/ToggleButtonGroup';
---
+++ src/entities/toggleButtonGroup/ui/ToggleButtonGroup.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import { ToggleButton, ToggleButtonGroup } from '@mui/material';
+
+import { Text } from '@shared/ui';
+import css from './tbg.module.css';
+
+type optionsParams = { key: string; buttonValue: string };
+
+type toggleBGProps = {
+  value: string;
+  onSetValue: (value: string) => void;
+  title: string;
+  params: optionsParams[];
+};
+
+export const ToggleButtonGroupComponent = (props: toggleBGProps) => {
+  const { value, onSetValue, title, params } = props;
+
+  const [currentValue, setCurrentValue] = useState(value);
+
+  const onChangeHandler = (event: React.MouseEvent, newValue: string) => {
+    if (newValue === currentValue || newValue === null) return;
+    setCurrentValue(newValue);
+    onSetValue(newValue);
+  };
+
+  return (
+    <div className={css.wrapper}>
+      <Text weight="bold" option="p" type="body">
+        {title}
+      </Text>
+      <ToggleButtonGroup
+        className={css.toggleGroup}
+        value={value}
+        onChange={onChangeHandler}
+        color="primary"
+        exclusive
+      >
+        {params.map(({ key, buttonValue }) => (
+          <ToggleButton value={key} key={key} className={css.btnGroup}>
+            {buttonValue}
+          </ToggleButton>
+        ))}
+      </ToggleButtonGroup>
+    </div>
+  );
+};
