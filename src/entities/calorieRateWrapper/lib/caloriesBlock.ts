4181b31d66241f0137105dee227d89aa5608d925
nino <dankozero111@gmail.com>
2024-11-04 18:45:22 +0300

    [fix]: update with funct, that returns array of object of passed data

--- src/entities/calorieRateWrapper/lib/caloriesBlock.ts
+++ src/entities/calorieRateWrapper/lib/caloriesBlock.ts
@@ -1,15 +1,7 @@
-export const calculateCalories = (values: number[]) => {
-  const [basicMet, rate] = values;
-  return [
-    {
-      value: basicMet,
-      title: 'Your basic metabolism',
-      text: 'These are calories that are burned when you are at rest, and energy is spent on ensuring the processes of respiration, blood circulation, maintaining body temperature, etc.',
-    },
-    {
-      value: rate,
-      title: 'Your calorie rate',
-      text: `Your calorie rate to maintain weight with current physical activity (you don't lose weight or gain weight)`,
-    },
-  ];
+export type listTYpe = { text: string; title: string }[];
+
+export const calculateCalories = (list: listTYpe, values: number[]) => {
+  return list.map((el, i) => {
+    return { ...el, value: values[i] };
+  });
 };
