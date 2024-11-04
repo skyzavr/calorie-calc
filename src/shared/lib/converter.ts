62814914b9dd72cb063fd9bfaf546303063660a0
nino <dankozero111@gmail.com>
2024-11-04 12:11:25 +0300

    [fix]: fix type alias

--- src/shared/lib/converter.ts
+++ src/shared/lib/converter.ts
@@ -1,10 +1,10 @@
 const pound = 0.45359237;
 const inch = 2.54;
 
-export const inchToCm = (inches: number) => {
+export const inchToCm = (inches: number | string): number => {
   return Number(inches) * inch;
 };
 
-export const poundToKilo = (pounds: number) => {
+export const poundToKilo = (pounds: number | string): number => {
   return Number(pounds) * pound;
 };
