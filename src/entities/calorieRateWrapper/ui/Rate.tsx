995060a44744e398485ac7ff2fe5c9d8b7fad1d2
nino <dankozero111@gmail.com>
2024-11-04 18:45:46 +0300

    [fix]: add translation

--- src/entities/calorieRateWrapper/ui/Rate.tsx
+++ src/entities/calorieRateWrapper/ui/Rate.tsx
@@ -1,17 +1,21 @@
-import { calculateCalories } from '../lib/caloriesBlock';
+import { TFunction } from 'i18next';
+
+import { calculateCalories, listTYpe } from '../lib/caloriesBlock';
 import { TextBlock } from '@entities/DescBlock';
 import { Text } from '@shared/ui';
 
 import css from './rate.module.css';
 
-type rateProps = { metabolism: number; rate: number };
-export const Rate = ({ metabolism, rate }: rateProps) => {
-  const textBlocks = calculateCalories([metabolism, rate]);
+type rateProps = { metabolism: number; rate: number; t: TFunction };
+
+export const Rate = ({ metabolism, rate, t }: rateProps) => {
+  const list = t('rateWrapper.list', { returnObjects: true }) as listTYpe;
+  const textBlocks = calculateCalories(list, [metabolism, rate]);
 
   return (
     <div className={css.rateWrapper}>
       <Text type="h3" weight="medium" option="h3">
-        Calories rate
+        {t('header')}
       </Text>
       {textBlocks.map((el) => (
         <TextBlock {...el} key={el.title} />
