4c06af066468a6056c1c76148916c2025780b3c6
nino <dankozero111@gmail.com>
2024-11-04 12:10:07 +0300

    [fix]: updaate functions with type alias && dender arg

--- src/pages/result/lib/metabolismCalc.ts
+++ src/pages/result/lib/metabolismCalc.ts
@@ -1,19 +1,20 @@
 type getMetabType = (
   correctWeight: number,
   correctHeigh: number,
-  age: number,
-  genderParam: number
+  age: number | string,
+  gender: string | number
 ) => number;
-type rateType = (metabolism: number, activity: string) => number;
+type rateType = (metabolism: number, activity: number | string) => number;
 
-export const getMetabolism: getMetabType = (
-  correctWeight,
-  correctHeight,
-  age,
-  genderParam
-) => {
+export const getMetabolism: getMetabType = (...args) => {
+  const [correctWeight, correctHeight, age, gender] = args;
+  const genderParam = gender === 'female' ? -161 : 5;
+
   const metabolism =
-    9.99 * correctWeight + 6.25 * correctHeight - 4.92 * age + genderParam;
+    9.99 * correctWeight +
+    6.25 * correctHeight -
+    4.92 * Number(age) +
+    genderParam;
   return Number(metabolism.toFixed(0));
 };
 export const getRate: rateType = (metabolism, activity) =>
